d5c9627af4dc1a68bf6be8881a330e20ad08f6df
woojongn <gkdldnwhd159@gmail.com>
2024-05-22 09:40:21 +0900

    남우종 - MyPageAuthenticationService 수정, Test파일 수정

--- src/main/java/com/gcf/spring/service/MyPageAuthenticationService.java
+++ src/main/java/com/gcf/spring/service/MyPageAuthenticationService.java
@@ -1,33 +1,23 @@
 package com.gcf.spring.service;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 import com.gcf.spring.dto.MyPageAuthenticationRequest;
-import com.gcf.spring.entity.Member;
 import com.gcf.spring.repository.MemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
-@Service
 public class MyPageAuthenticationService {
 
     private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    @Autowired
-    public MyPageAuthenticationService(MemberRepository memberRepository) {
+    public MyPageAuthenticationService(MemberRepository memberRepository, PasswordEncoder passwordEncoder) {
         this.memberRepository = memberRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
-    public boolean authenticate(MyPageAuthenticationRequest authenticationRequest) {
-        String id = authenticationRequest.getId();
-        String password = authenticationRequest.getPassword();
-
-        Optional<Member> optionalMember = memberRepository.findById(id);
-        if (optionalMember.isPresent()) {
-            Member member = optionalMember.get();
-            return member.getPassword().equals(password);
-        }
-
-        return false;
+    public boolean authenticate(MyPageAuthenticationRequest request) {
+        return memberRepository.findById(request.getId())
+                .map(member -> passwordEncoder.matches(request.getPassword(), member.getPassword()))
+                .orElse(false);
     }
 }
