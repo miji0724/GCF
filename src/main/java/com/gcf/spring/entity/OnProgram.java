e74260e47cbef86a6785253cb07580571c3d52f7
woojongn <gkdldnwhd159@gmail.com>
2024-06-10 16:22:48 +0900

    OnProgramDto, entity 수정

--- src/main/java/com/gcf/spring/entity/OnProgram.java
+++ src/main/java/com/gcf/spring/entity/OnProgram.java
@@ -24,67 +24,66 @@ import lombok.Setter;
 @Getter
 @Setter
 public class OnProgram {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "id", nullable = false)
-	private Integer id; // 프로그램 번호
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id; // 프로그램 번호
 
-	@ManyToOne
-	@JoinColumn(name = "teacher_id", referencedColumnName = "id")
-	private Teacher teacher; // 강사 ID
+    @ManyToOne
+    @JoinColumn(name = "teacher_id", referencedColumnName = "id")
+    private Teacher teacher; // 강사 ID
 
-	@Column(name = "on_program_name", nullable = false)
-	private String programName; // 프로그램 이름
+    @Column(name = "on_program_name", nullable = false)
+    private String programName; // 프로그램 이름
 
-	@Column(name = "operating_start_day", nullable = false)
-	private LocalDate operatingStartDay; // 업로드 날짜
+    @Column(name = "operating_start_day", nullable = false)
+    private LocalDate operatingStartDay; // 업로드 날짜
 
-	@Column(name = "views")
-	private Integer views = 0; // 조회수
+    @Column(name = "views")
+    private Integer views = 0; // 조회수
 
-	@Column(name = "likes_count", nullable = false)
-	private Integer likesCount = 0; // 좋아요 수
+    @Column(name = "likes_count", nullable = false)
+    private Integer likesCount = 0; // 좋아요 수
 
-	@Column(name = "online_category", nullable = false)
-	private String category; // 프로그램 카테고리
+    @Column(name = "online_category", nullable = false)
+    private String category; // 프로그램 카테고리
 
-	@Column(name = "program_type", nullable = false)
-	private String programType; // 프로그램 타입 (온라인/오프라인 구분)
+    @Column(name = "program_type", nullable = false)
+    private String programType; // 프로그램 타입 (온라인/오프라인 구분)
 
-	@OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
-	@JoinColumn(name = "poster_id", referencedColumnName = "id")
-	private Attachment poster; // 포스터 정보
-	
-	@Column(nullable = false)
-	private String approvalState; // 승인, 미승인, 승인대기
+    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "poster_id", referencedColumnName = "id")
+    private Attachment poster; // 포스터 정보
 
-	@OneToMany(mappedBy = "onProgram", cascade = CascadeType.ALL)
-	private List<ProgramInfo> programInfos;
+    @Column(nullable = false)
+    private String approvalState; // 승인, 미승인, 승인대기
 
-	@OneToMany(mappedBy = "onProgram", cascade = CascadeType.ALL)
-	private List<TeacherInfo> teacherInfos;
+    @OneToMany(mappedBy = "onProgram", cascade = CascadeType.ALL)
+    private List<ProgramInfo> programInfos;
 
-	@OneToMany(mappedBy = "postId", cascade = CascadeType.ALL)
-	private List<Comment> comments; // 댓글 리스트
+    @OneToMany(mappedBy = "onProgram", cascade = CascadeType.ALL)
+    private List<TeacherInfo> teacherInfos;
 
-	@OneToMany(mappedBy = "onProgram", cascade = CascadeType.ALL)
-	private List<OnVideo> videos; // 비디오 리스트
-	
-	public static OnProgram createOnProgram(OnProgramDto on_programDto) {
-		OnProgram onProgram = new OnProgram();
-		onProgram.setTeacher(on_programDto.getTeacher());
-		onProgram.setProgramName(on_programDto.getProgramName());
-		onProgram.setOperatingStartDay(on_programDto.getOperatingStartDay());
-		onProgram.setViews(on_programDto.getViews());
-		onProgram.setLikesCount(on_programDto.getLikesCount());
-		onProgram.setCategory(on_programDto.getOnlineCategory());
-		onProgram.setProgramType(on_programDto.getProgramType());
-		onProgram.setProgramInfos(on_programDto.getProgramInfos());
-		onProgram.setTeacherInfos(on_programDto.getTeacherInfos());
-		onProgram.setPoster(on_programDto.getPoster());
-		onProgram.setComments(on_programDto.getComments());
-		onProgram.setApprovalState(on_programDto.getApprovalState());
-		return onProgram;
+    @OneToMany(mappedBy = "postId", cascade = CascadeType.ALL)
+    private List<Comment> comments; // 댓글 리스트
 
-	}
+    @OneToMany(mappedBy = "onProgram", cascade = CascadeType.ALL)
+    private List<OnVideo> videos; // 비디오 리스트
+
+    public static OnProgram createOnProgram(OnProgramDto on_programDto) {
+        OnProgram onProgram = new OnProgram();
+        onProgram.setTeacher(on_programDto.getTeacher());
+        onProgram.setProgramName(on_programDto.getProgramName());
+        onProgram.setOperatingStartDay(on_programDto.getOperatingStartDay());
+        onProgram.setViews(on_programDto.getViews());
+        onProgram.setLikesCount(on_programDto.getLikesCount());
+        onProgram.setCategory(on_programDto.getCategory()); // 필드명 변경
+        onProgram.setProgramType(on_programDto.getProgramType());
+        onProgram.setProgramInfos(on_programDto.getProgramInfos());
+        onProgram.setTeacherInfos(on_programDto.getTeacherInfos());
+        onProgram.setPoster(on_programDto.getPoster());
+        onProgram.setComments(on_programDto.getComments());
+        onProgram.setApprovalState(on_programDto.getApprovalState());
+        return onProgram;
+    }
 }
