1b95cab0d573370197bf0cc75b831b75e9b9bcfc
DESKTOP-TPVN74N\big601-03 <audwo1245@gmail.com>
2024-06-13 20:01:35 +0900

    master 2

--- src/main/java/com/gcf/spring/config/GoogleCloudStorageConfig.java
+++ src/main/java/com/gcf/spring/config/GoogleCloudStorageConfig.java
@@ -1,12 +1,14 @@
 package com.gcf.spring.config;
 
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.cloud.storage.Storage;
-import com.google.cloud.storage.StorageOptions;
+import java.io.IOException;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
-import java.io.IOException;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
 
 @Configuration
 public class GoogleCloudStorageConfig {
