d67a7bad500697bbb78ebed31e9392180bac40d2
woojongn <gkdldnwhd159@gmail.com>
2024-06-13 15:40:48 +0900

    자신이 단 코멘트 가져오기

--- src/main/java/com/gcf/spring/controller/CommentController.java
+++ src/main/java/com/gcf/spring/controller/CommentController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @RestController
@@ -17,7 +18,11 @@ public class CommentController {
     private CommentRepository commentRepository;
 
     @GetMapping("/comments")
-    public List<Comment> getAllComments() {
-        return commentRepository.findAll();
+    public List<Comment> getAllComments(HttpServletRequest request) {
+        String userId = (String) request.getSession().getAttribute("userId");
+        if (userId == null) {
+            throw new RuntimeException("User not authenticated");
+        }
+        return commentRepository.findByMemberId(userId);
     }
 }
--- src/main/java/com/gcf/spring/repository/CommentRepository.java
+++ src/main/java/com/gcf/spring/repository/CommentRepository.java
@@ -1,7 +1,9 @@
 package com.gcf.spring.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.gcf.spring.entity.Comment;
 
 public interface CommentRepository extends JpaRepository<Comment, Integer> {
+    List<Comment> findByMemberId(String memberId);
 }
